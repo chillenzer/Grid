554201c98c323fc55224cd86bfe8ae3bbdca5099
Julian Lenz <j.j.lenz@swansea.ac.uk>
2022-09-14 14:48:35 +0100

    Added another test and logic around n_gauge

--- Grid/parallelIO/HiRepIO.h
+++ Grid/parallelIO/HiRepIO.h
@@ -1,14 +1,39 @@
 #include <sstream>
 
 namespace Grid {
+enum class endianness {
+  little = 0,
+  big = 1,
+};
+
+inline endianness system_endianess() {
+  const int value{0x01};
+  const void *address = static_cast<const void *>(&value);
+  const unsigned char *least_significant_address =
+      static_cast<const unsigned char *>(address);
+  return (*least_significant_address == 0x01) ? endianness::little
+                                              : endianness::big;
+}
+
+template <class T>
+void endswap(T *objp) {
+  unsigned char *memp = reinterpret_cast<unsigned char *>(objp);
+  std::reverse(memp, memp + sizeof(T));
+}
+
 struct HiRepHeaderData {
   int N_gauge = 0;
 };
 class HiRepIO {
  public:
   static HiRepHeaderData readHeader(std::stringstream stream) {
+    int n_gauge;
+    stream.read(reinterpret_cast<char *>(&n_gauge), 4);
+    if (system_endianess() == endianness::little) {
+      endswap(&n_gauge);
+    }
     HiRepHeaderData header;
-    header.N_gauge = 42;
+    header.N_gauge = n_gauge;
     return header;
   };
 };
--- tests/IO/Test_hirep_read.cc
+++ tests/IO/Test_hirep_read.cc
@@ -5,27 +5,6 @@
 #include <sstream>
 
 #include "Grid/parallelIO/HiRepIO.h"
-
-enum class endianness {
-  little = 0,
-  big = 1,
-};
-
-inline endianness system_endianess() {
-  const int value{0x01};
-  const void *address = static_cast<const void *>(&value);
-  const unsigned char *least_significant_address =
-      static_cast<const unsigned char *>(address);
-  return (*least_significant_address == 0x01) ? endianness::little
-                                              : endianness::big;
-}
-
-template <class T>
-void endswap(T *objp) {
-  unsigned char *memp = reinterpret_cast<unsigned char *>(objp);
-  std::reverse(memp, memp + sizeof(T));
-}
-
 struct HeaderStreamGenerator {
   uint32_t n_gauge = 0;
   uint32_t n_t = 0;
@@ -42,13 +21,13 @@ struct HeaderStreamGenerator {
     uint32_t n_z_tmp = n_z;
     double plaquette_tmp = plaquette;
 
-    if (system_endianess() == endianness::little) {
-      endswap(&n_gauge_tmp);
-      endswap(&n_t_tmp);
-      endswap(&n_x_tmp);
-      endswap(&n_y_tmp);
-      endswap(&n_z_tmp);
-      endswap(&plaquette_tmp);
+    if (Grid::system_endianess() == Grid::endianness::little) {
+      Grid::endswap(&n_gauge_tmp);
+      Grid::endswap(&n_t_tmp);
+      Grid::endswap(&n_x_tmp);
+      Grid::endswap(&n_y_tmp);
+      Grid::endswap(&n_z_tmp);
+      Grid::endswap(&plaquette_tmp);
     }
 
     std::stringstream stream;
@@ -72,7 +51,16 @@ void test_reads_n_gauge_from_header() {
   assert(header.N_gauge == N_gauge);
 }
 
+void test_reads_another_n_gauge_from_header() {
+  HeaderStreamGenerator stream_gen;
+  int N_gauge = 7;
+  stream_gen.n_gauge = N_gauge;
+  Grid::HiRepHeaderData header = Grid::HiRepIO::readHeader(stream_gen());
+  assert(header.N_gauge == N_gauge);
+}
+
 int main() {
   test_reads_n_gauge_from_header();
+  test_reads_another_n_gauge_from_header();
   return 0;
 }
